f6dada7e672cb1885627464f58661ade1962c7c4
Ann Zhang <ann.zhang01@sap.com>
2018-04-12 14:05:16 +0800

    add sqlite

--- App.js
+++ App.js
@@ -9,8 +9,11 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList,
+  Button
 } from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,12 +22,49 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: [],
+      db: null
+    };
+  }
+
+  errorCB(err) {
+    console.log("SQL Error: " + err);
+  }
+  
+  successCB() {
+    console.log("SQL executed fine");
+  }
+  
+  openCB() {
+    console.log("Database OPENED");
+  }
+
+  componentWillMount() {
+    var db = SQLite.openDatabase("test.db", "1.0", "Test Database", 200000, this.openCB, this.errorCB);
+    if (db) {
+      db.transaction(tx => {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Person(id REAL UNIQUE, name TEXT)', [], (tx, results) => {
+          console.log("create success");
+
+          this.setState({
+            db: db
+          });
+          this.displayData();
+        }, (tx, error) => {
+          alert("Fail to create database. " + error.message);
+        })
+      });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        {/* <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
@@ -32,9 +72,89 @@ export default class App extends Component<Props> {
         </Text>
         <Text style={styles.instructions}>
           {instructions}
-        </Text>
+        </Text> */}
+        <Button style={{marginTop: 64}}
+          title="Add"
+          onPress={this.onPressAdd.bind(this)}/>
+        <Button
+          title="Clean"
+          onPress={this.onPressClean.bind(this)}/>
+        <FlatList
+          data={this.state.person}
+          renderItem={({item}) => <Text>{item.key + ', ' +item.name}</Text>}
+        />
       </View>
     );
+  }
+
+  onPressAdd() {
+    var db = this.state.db;
+    var that = this;
+    let insertStatement = "INSERT INTO Person (id, name) VALUES (?, ?)";
+    var startTime, endTime;
+    db.transaction(tx => {
+      startTime = new Date();
+      for (var i = 0; i < 10; i++) {
+        console.log("start insert " + i);
+        tx.executeSql(insertStatement, [i, "Zhang-"+i], (tx, results)=> {
+          console.log("finish insert " + i );
+        });
+      }
+      // tx.executeSql(insertStatement, [1, "A"]);
+      // that.displayData();
+      // tx.executeSql("SELECT COUNT(*) AS c FROM Person", [], (tx, results) => {
+      //   let index = results.rows.item(0).c + 2;
+      //   tx.executeSql("INSERT INTO Person (id, name) VALUES (?, ?)", [index, 'Zhang-' + index], (tx, results) => {
+      //     that.displayData();
+      //   }, (tx, error) => {
+          
+      //   });
+      // });
+    }, () => {
+    }, () => {
+      endTime = new Date();
+      let str = "batch insert finish, " + (endTime - startTime)/1000 + "s";
+      console.log(str);
+      // alert(str);
+      // that.displayData();
+    });
+    console.log("press finish");
+  }
+
+  onPressClean() {
+    var db = this.state.db;
+    var that = this;
+    db.transaction(tx => {
+      tx.executeSql("DELETE FROM Person", [], (tx, results) => {
+          that.displayData();
+        }, (tx, error) => {
+          alert("Delete error: " + error.message);
+        });
+    });
+  }
+
+  displayData() {
+    var db = this.state.db;
+    var that = this;
+    db.transaction((tx) => {
+      tx.executeSql('SELECT * FROM Person', [], (tx, results) => {
+          console.log("Query completed");
+          var person = [];
+          var len = results.rows.length, i, item;
+          for (i = 0; i < len; i++){
+            item = results.rows.item(i);
+            person.push({
+              key: ""+item.id,
+              name: item.name
+            });
+          }
+          that.setState({
+            person: person
+          });
+        }, (tx, error) => {
+          alert("Error");
+        });
+    });
   }
 }
 
@@ -55,4 +175,7 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  list: {
+    marginTop: 64,
+  }
 });
